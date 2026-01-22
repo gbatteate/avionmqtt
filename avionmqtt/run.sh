0240865c0adfbb6167997065b74c37b664107fe0
gbatteate <106771657+gbatteate@users.noreply.github.com>
2026-01-21 21:23:34 -0800

    Update run.sh

--- avionmqtt/run.sh
+++ avionmqtt/run.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
 
+LOG_LEVEL=$(bashio::config 'log_level')
+
 if [ ! -f /config/avionmqtt/settings.yaml ]; then
-  echo "ERROR: /config/avionmqtt/settings.yaml not found"
+  echo "ERROR: Missing /config/avionmqtt/settings.yaml"
   exit 1
 fi
 
-echo "Starting avionmqtt..."
-avionmqtt -s /config/avionmqtt/settings.yaml --log=INFO
+echo "Starting avionmqtt (log=${LOG_LEVEL})"
+exec /opt/venv/bin/avionmqtt -s /config/avionmqtt/settings.yaml --log=${LOG_LEVEL}
+
